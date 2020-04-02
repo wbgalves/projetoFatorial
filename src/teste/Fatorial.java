422cda0758a7b7ac5054d6a8c4f6537a8d095a8d
Willian Alves <willian.alves@uncisal.edu.br>
2020-04-02 09:39:15 -0300

    Ok google

--- src/teste/Fatorial.java
+++ src/teste/Fatorial.java
@@ -1,15 +1,15 @@
 package teste;
 
 public class Fatorial {
-    private static int calcularFatorial(int i) {
+    /**
+     * Versão recursiva do cálculo do fatorial
+     * @param i
+     * @return
+     */
+    public static int calcularFatorial(int i) {
         if (i > 1) {
             return i * calcularFatorial(i-1);
         } 
         return 1;
     }
-
-    public static void main(String[] args) {
-        System.out.println(Fatorial.calcularFatorial(5));
-    }
-
 }
---
+++ src/teste/TesteFatorial.java
@@ -0,0 +1,8 @@
+package teste;
+
+public class TesteFatorial {
+    public static void main(String[] args) {
+        int resultado = Fatorial.calcularFatorial(5);
+        System.out.println(resultado);
+    }
+}
