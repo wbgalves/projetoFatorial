899ce1851ae89cb786f15e9dfe32fb7610eeb68b
Willian Alves <willian.alves@uncisal.edu.br>
2020-04-04 18:02:47 -0300

    Calculo do fatorial de forma iterativa

--- src/teste/Fatorial.java
+++ src/teste/Fatorial.java
@@ -15,4 +15,14 @@ public class Fatorial {
         } 
         return 1;
     }
+
+    public int calcularFatorialIterativo(int n) throws IllegalArgumentException {
+        if (n < 0)
+            throw new IllegalArgumentException("Não existe fatorial de número negativo");
+        int resultado = 1;
+        for(int i = 2; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
 }
