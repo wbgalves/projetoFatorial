3c206d0026b121cecff2c1206420cc96f74c0434
Willian Alves <willian.alves@uncisal.edu.br>
2020-04-05 22:21:35 -0300

    Mudanca do valor do parametro para calculo do fatorial

--- src/teste/TesteFatorial.java
+++ src/teste/TesteFatorial.java
@@ -3,7 +3,7 @@ package teste;
 public class TesteFatorial {
     public static void main(String[] args) {
         Fatorial f = new Fatorial();
-        int resultado = f.calcularFatorial(5);
+        int resultado = f.calcularFatorial(3);
         System.out.println(resultado);
     }
 }
